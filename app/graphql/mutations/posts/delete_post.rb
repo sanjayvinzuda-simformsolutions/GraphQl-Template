6d1a70df59d2ecdb327af18bd69fe72f6a823884
rahulgupta <rahul.g@simformsolutions.com>
2023-05-29 14:53:39 +0530

    GraphQL Post edit and delete authorization [29/05/2023] modify delete_post.rb file for authorized post delete modfiy update_post.rb file for authorized post update

--- app/graphql/mutations/posts/delete_post.rb
+++ app/graphql/mutations/posts/delete_post.rb
@@ -5,16 +5,19 @@ module Mutations
 
     # TODO: define arguments
     argument :id, ID, required: true
-
+    argument :user_id, Integer, required: true
     # TODO: define resolve method
-    def resolve(id:)
-      post = Post.find(id)
-      post.destroy
-      {
-        post: post
-      }
-    rescue ActiveRecord::RecordNotFound => e 
-      raise GraphQL::ExecutionError.new(e.message)
+    def resolve(id:,user_id:)
+      post = Post.find_by(id: id)
+      
+      raise GraphQL::ExecutionError.new("Record Not Found") if post.nil?
+      if post.user_id == user_id
+        post.destroy
+        { post: post }
+      else
+        { post: nil }
+        raise GraphQL::ExecutionError.new("You are not authorized to delete this post")
+      end
     end
   end
 end
--- app/graphql/mutations/posts/update_post.rb
+++ app/graphql/mutations/posts/update_post.rb
@@ -5,15 +5,19 @@ module Mutations
 
     # TODO: define arguments
     argument :attributes, Types::Posts::UpdatepostattributesInputType, required: true
+    field :errors, [String], null: false
     
 
     # TODO: define resolve method
     def resolve(attributes:)
-      post = Post.find(attributes.id)
-    if post.update(title: attributes.title, body: attributes.body, user_id: attributes.user_id)
-          { post: post }
+      post = Post.find_by(id: attributes.id)
+      raise GraphQL::ExecutionError.new("Record Doesn't exists.") if post.nil?
+      if post.user_id == attributes.user_id 
+        post.update(title: attributes.title, body: attributes.body, user_id: attributes.user_id)
+        { post: post, errors: [] }
       else
-          {  post: nil }
+            post.errors.add(:permission, "You are not the owner of this post.")
+          {  post: nil, errors: post.errors.full_messages }
       end
     end
   end
